6bd5e2a25aaf91ae863b93356e5bd6be5030a383
bharani <bharani_s@yahoo.com>
2010-05-31 16:37:15 +0530

    Added hibernate test base classs

--- GetIn/ProfileTest.cs
+++ GetIn/ProfileTest.cs
@@ -2,13 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NHibernate;
 using NUnit.Framework;
 
 namespace GetIn
 {
     [TestFixture]
-    public class ProfileTest
+    public class ProfileTest : NHibernateInMemoryTestFixtureBase
     {
+        private ISession session;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            InitalizeSessionFactory(new FileInfo("src/User.hbm.xml"), new FileInfo("src/Profile.hbm.xml"));
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            session = this.CreateSession();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            session.Dispose();
+        }
         [Test]
         public void ShouldBeAbleToCreateProfile(){
             Profile profile = new Profile("Some useful profile goes here");
