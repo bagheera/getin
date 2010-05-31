4045dc46048f05873cc36cfe8dcedfaebebc7ae7
bharani <bharani_s@yahoo.com>
2010-05-31 16:21:47 +0530

    Added users and profiles

---
+++ GetIn/Profile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GetIn
+{
+    public class Profile
+    {
+        private String profile;
+
+        public Profile(string givenProfile)
+        {
+            profile = givenProfile;
+        }
+
+        public override string ToString()
+        {
+            return profile;
+        }
+    }
+}
