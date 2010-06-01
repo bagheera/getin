c248ce82a8beb53c1c760e833abf91e54c8e442c
bharani <bharani_s@yahoo.com>
2010-06-01 15:31:36 +0530

    Test profile with 100kb and with more than 100kb text content

--- GetIn/Profile.cs
+++ GetIn/Profile.cs
@@ -3,32 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace GetIn
-{
-    public class Profile
-    {
+namespace GetIn{
+    public class Profile{
+        private const int MAX_PROFILE_LENGTH = 102400;
         private String profileText;
 
         public Profile(){
-            
         }
-        public Profile(string givenProfile)
-        {
+
+        public Profile(string givenProfile){
+            if (givenProfile.Length > MAX_PROFILE_LENGTH){
+                givenProfile = givenProfile.Substring(0, MAX_PROFILE_LENGTH);
+            }
             profileText = givenProfile;
         }
 
-        public String ProfileText
-        {
+        public String ProfileText{
             get { return profileText; }
             set { profileText = value; }
         }
 
-        public override string ToString()
-        {
+        public override string ToString(){
             return profileText;
         }
-        public override bool Equals(object obj)
-        {
+
+        public override bool Equals(object obj){
             return base.Equals(obj);
         }
     }
