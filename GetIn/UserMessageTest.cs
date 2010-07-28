5517b1ec7ded692cd4b5e7b02b8194987f2c9fe8
vinayvenu <ijkvinay@gmail.com>
2010-07-28 17:19:06 +0530

    added new files

---
+++ GetIn/UserMessageTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Moq;
+using NUnit.Framework;
+
+namespace GetIn
+{
+    class UserMessageTest
+    {
+        [Test]
+        public void UserCanPostToSubscribedGroup()
+        {
+            Mock<Group> mockGroup = new Mock<Group>();
+            Post post = new Post() {Content = "Some stupid content", PostedDateTime = new DateTime(2010, 01, 01)};
+            mockGroup.Setup(foo => foo.post(post));
+
+            LoginId loginid = new LoginId("test@ThoughtWorks.com");
+            string firstname = "firstName";
+            string lastname = "lastName";
+            Name name = new Name(firstname, lastname);
+
+            User footballFan = new User(loginid, name);
+            //footballFan.
+
+        }
+
+        [Test]
+        public void UserCannotPostToUnsubscribedGroup()
+        {
+//            Assert.AreEqual("expected", "actual");
+        }
+
+        [Test]
+        public void GroupCanListAllMessagsPostedToIt()
+        {
+//            Assert.AreEqual("expected", "actual");
+        }
+    }
+}
